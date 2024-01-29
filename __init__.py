338a3c851d3a5dc65cde575fe57d596f2bd86018
5h1q1 <232447K@mymail.nyp.edu.sg>
2024-01-29 16:41:46 +0800

    test again

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 import mailbox
 import bcrypt as Bcrypt
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from main import *
-import os
+import os, shelve, Response, Product
+from Form import CreateCheckoutForm, CreateUpdateForm, CreateProductForm
 
 
 def create_app():
@@ -274,6 +275,209 @@ def reply_review():
 
      return render_template('productmanagement.html', count=len(reviews_list), reviews_list=reviews_list)
 
+
+@app.route('/')
+# get method to get donation page
+def donation():
+    return render_template("donation.html")
+
+
+@app.route('/success')
+def success():
+    return render_template("success.html")
+
+
+@app.route('/checkout', methods=['GET', 'POST'])
+# accepts both get and post methods, checkout page is retrieved
+# when form is received, data will be posted onto the server
+def response():
+    # Retrieve the 'amount' parameter
+    amount = request.args.get('amount')
+    create_checkout_form = CreateCheckoutForm(request.form)  # class object, calls server and passes in request
+    default_amount = amount
+    # Set the default value for amount
+    create_checkout_form.amount.data = default_amount
+
+    if request.method == 'POST' and create_checkout_form.validate():
+        responses_dict = {}
+        db = shelve.open('response.db', 'c')
+
+        try:
+            responses_dict = db['Responses']
+        except:
+            print("Error in retrieving Responses from response.db.")
+
+        response = Response.Response(create_checkout_form.fname.data, create_checkout_form.lname.data,
+                                     create_checkout_form.phone.data, create_checkout_form.email.data,
+                                     create_checkout_form.add1.data, create_checkout_form.add2.data,
+                                     create_checkout_form.pcode.data, create_checkout_form.dmethod.data,
+                                     create_checkout_form.amount.data)
+        responses_dict[response.get_response_id()] = response
+
+        db['Responses'] = responses_dict
+
+        db.close()
+
+        return redirect(url_for('success'))
+
+    return render_template('checkout.html', form=create_checkout_form, customAmount=amount)
+
+
+@app.route('/responseManagement')
+def response_management():
+    responses_dict = {}
+    db = shelve.open('response.db', 'r')
+    responses_dict = db['Responses']
+    db.close()
+
+    responses_list = []
+    for key in responses_dict:
+        response = responses_dict.get(key)
+        responses_list.append(response)
+
+    return render_template('responseManagement.html', count=len(responses_list), responses_list=responses_list)
+
+
+@app.route('/updateResponse/<int:id>/', methods=['GET', 'POST'])
+def update_response(id):
+    update_checkout_form = CreateUpdateForm(request.form)
+    if request.method == 'POST' and update_checkout_form.validate():
+        responses_dict = {}
+        db = shelve.open('response.db', 'w')
+        responses_dict = db['Responses']
+
+        response = responses_dict.get(id)
+        response.set_fname(update_checkout_form.fname.data)
+        response.set_lname(update_checkout_form.lname.data)
+        response.set_phone(update_checkout_form.phone.data)
+        response.set_email(update_checkout_form.email.data)
+        response.set_add1(update_checkout_form.add1.data)
+        response.set_add2(update_checkout_form.add2.data)
+        response.set_pcode(update_checkout_form.pcode.data)
+        response.set_dmethod(update_checkout_form.dmethod.data)
+
+        db['Responses'] = responses_dict
+        db.close()
+
+        return redirect(url_for('response_management'))
+    else:
+        responses_dict = {}
+        db = shelve.open('response.db', 'r')
+        responses_dict = db['Responses']
+        db.close()
+
+        response = responses_dict.get(id)
+        update_checkout_form.fname.data = response.get_fname()
+        update_checkout_form.lname.data = response.get_lname()
+        update_checkout_form.phone.data = response.get_phone()
+        update_checkout_form.email.data = response.get_email()
+        update_checkout_form.add1.data = response.get_add1()
+        update_checkout_form.add2.data = response.get_add2()
+        update_checkout_form.pcode.data = response.get_pcode()
+        update_checkout_form.dmethod.data = response.get_dmethod()
+
+    return render_template('updateResponse.html', form=update_checkout_form)
+
+
+@app.route('/deleteResponse/<int:id>', methods=['POST'])
+def delete_response(id):
+    responses_dict = {}
+    db = shelve.open('response.db', 'w')
+    responses_dict = db['Responses']
+
+    responses_dict.pop(id)
+
+    db['Responses'] = responses_dict
+    db.close()
+
+    return redirect(url_for('response_management'))
+
+
+@app.route('/createProduct', methods=['GET', 'POST'])
+# accepts both get and post methods, createProduct page is retrieved
+# when form is received, data will be posted onto the server
+def create_product():
+    create_product_form = CreateProductForm(request.form)  # class object, calls server and passes in request
+    if request.method == 'POST' and create_product_form.validate():
+        products_dict = {}
+        db = shelve.open('product.db', 'c')
+
+        try:
+            products_dict = db['Products']
+        except:
+            print("Error in retrieving Products from product.db.")
+
+        product = Product.Product(create_product_form.name.data, create_product_form.price.data,
+                         create_product_form.description.data, create_product_form.tags.data)
+        products_dict[product.get_product_id()] = product
+        db['Products'] = products_dict
+
+        db.close()
+
+        return redirect(url_for('product_management'))
+    return render_template('createProduct.html', form=create_product_form)
+
+
+@app.route('/productManagement')
+def product_management():
+    products_dict = {}
+    db = shelve.open('product.db', 'r')
+    products_dict = db['Products']
+    db.close()
+
+    products_list = []
+    for key in products_dict:
+        product = products_dict.get(key)
+        products_list.append(product)
+
+    return render_template('productManagement.html', count=len(products_list), products_list=products_list)
+
+
+@app.route('/updateProduct/<int:id>/', methods=['GET', 'POST'])
+def update_product(id):
+    update_product_form = CreateProductForm(request.form)
+    if request.method == 'POST' and update_product_form.validate():
+        products_dict = {}
+        db = shelve.open('product.db', 'w')
+        products_dict = db['Products']
+
+        product = products_dict.get(id)
+        product.set_name(update_product_form.name.data)
+        product.set_price(update_product_form.price.data)
+        product.set_description(update_product_form.description.data)
+        product.set_tags(update_product_form.tags.data)
+
+        db['Products'] = products_dict
+        db.close()
+
+        return redirect(url_for('product_management'))
+    else:
+        products_dict = {}
+        db = shelve.open('product.db', 'r')
+        products_dict = db['Products']
+        db.close()
+
+        product = products_dict.get(id)
+        update_product_form.name.data = product.get_name()
+        update_product_form.price.data = product.get_price()
+        update_product_form.description.data = product.get_description()
+        update_product_form.tags.data = product.get_tags()
+
+        return render_template('updateProduct.html', form=update_product_form)
+
+
+@app.route('/deleteProduct/<int:id>', methods=['POST'])
+def delete_product(id):
+    products_dict = {}
+    db = shelve.open('product.db', 'w')
+    products_dict = db['Products']
+
+    products_dict.pop(id)
+
+    db['Products'] = products_dict
+    db.close()
+
+    return redirect(url_for('product_management'))
 
 
 if __name__ == '__main__':
