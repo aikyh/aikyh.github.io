3f857a4e0877fbb83941fa04778154e158030fb1
aikyh <aikyh02070208@gmail.com>
2024-01-31 17:37:08 +0800

    adding

--- Forms.py
+++ Forms.py
@@ -1,6 +1,6 @@
 from wtforms import (Form, StringField, RadioField, SelectField, TextAreaField, validators, PasswordField, IntegerField,
                      FloatField)
-from wtforms.fields import  DateField, TimeField, IntegerField
+from wtforms.fields import DateField, TimeField, IntegerField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Length, ValidationError
 
