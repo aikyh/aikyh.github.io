626c3b96b7792369eaa9e567ed0cabb24764ba39
5h1q1 <232447K@mymail.nyp.edu.sg>
2024-02-08 17:24:41 +0800

    idk

--- __init__.py
+++ __init__.py
@@ -414,6 +414,11 @@ def success():
     return render_template("success.html")
 
 
+@app.route('/productManagement')
+def productManagement():
+    return render_template("productManagement.html")
+
+
 @app.route('/checkout', methods=['GET', 'POST'])
 # accepts both get and post methods, checkout page is retrieved
 # when form is received, data will be posted onto the server
